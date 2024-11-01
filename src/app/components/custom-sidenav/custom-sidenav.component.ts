3f616d0a184ca6780805a4efe5491e443d37e2ac
deepakkumar007 <deepak.kumar@gigaforce.io>
2024-11-02 01:00:02 +0530

    icon fixed

--- src/app/components/custom-sidenav/custom-sidenav.component.ts
+++ src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -124,7 +124,7 @@ export class CustomSidenavComponent {
     {
       icon: 'settings',
       label: 'Settings',
-      route: 'services',
+      route: 'settings',
     },
     {
       icon: 'help',
