3d823c14431898361711e5dc5cf3b15634454b8a
deepakkumar007 <deepak.kumar@gigaforce.io>
2024-11-14 22:44:43 +0530

    text issue fixed

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -81,7 +81,7 @@ import { PerviewComponent } from '../../components/perview/perview.component';
 
             <mat-step>
               <ng-template matStepLabel>
-                <span>Skills &Experience</span>
+                <span>Skills & Experience</span>
               </ng-template>
               <app-skill-and-experience></app-skill-and-experience>
               <div class="button-container">
