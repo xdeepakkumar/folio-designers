ca7a754165450277894aecddb6b92c3557ab8ea4
deepakkumar007 <deepak.kumar@gigaforce.io>
2024-12-12 00:09:38 +0530

    different page issue fixed(imp)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {
   OnInit,
   HostListener,
 } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -35,85 +35,88 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent,
   ],
   template: `
-    <mat-toolbar class="mat-elevation-z3" color="primary">
-      <!-- Menu Button for Collapsing Sidebar -->
-      <button mat-icon-button (click)="toggleSidenav()">
-        <mat-icon>menu</mat-icon>
-      </button>
-
-      <!-- Company Name in Toolbar -->
-      <span class="company-name">Folio Designers</span>
-
-      <span class="spacer"></span>
-
-      <!-- Other Toolbar Content -->
-      <button mat-button *ngIf="isLargeScreen" (click)="navigateTo('about')">
-        About Us
-      </button>
-      <button mat-button *ngIf="isLargeScreen" (click)="navigateTo('contact')">
-        Contact Us
-      </button>
-
-      <!-- Notification and Profile Buttons with Menus -->
-      <button
-        mat-icon-button
-        matBadge=" "
-        matBadgeColor="warn"
-        matBadgeSize="small"
-        matBadgeOverlap="false"
-        [matMenuTriggerFor]="notificationMenu"
-      >
-        <mat-icon>notifications</mat-icon>
-      </button>
-      <mat-menu #notificationMenu="matMenu">
-        <button mat-menu-item>
-          <mat-icon>message</mat-icon> Notification 1
+    <ng-container *ngIf="showLayout">
+      <mat-toolbar class="mat-elevation-z3" color="primary">
+        <button mat-icon-button (click)="toggleSidenav()">
+          <mat-icon>menu</mat-icon>
         </button>
-        <button mat-menu-item>
-          <mat-icon>message</mat-icon> Notification 2
+        <span class="company-name">Folio Designers</span>
+        <span class="spacer"></span>
+        <button mat-button *ngIf="isLargeScreen" (click)="navigateTo('about')">
+          About Us
         </button>
-      </mat-menu>
+        <button
+          mat-button
+          *ngIf="isLargeScreen"
+          (click)="navigateTo('contact')"
+        >
+          Contact Us
+        </button>
+        <!-- Notification and Profile Buttons with Menus -->
+        <button
+          mat-icon-button
+          matBadge=" "
+          matBadgeColor="warn"
+          matBadgeSize="small"
+          matBadgeOverlap="false"
+          [matMenuTriggerFor]="notificationMenu"
+        >
+          <mat-icon>notifications</mat-icon>
+        </button>
+        <mat-menu #notificationMenu="matMenu">
+          <button mat-menu-item>
+            <mat-icon>message</mat-icon> Notification 1
+          </button>
+          <button mat-menu-item>
+            <mat-icon>message</mat-icon> Notification 2
+          </button>
+        </mat-menu>
+        <button mat-icon-button [matMenuTriggerFor]="profileMenu">
+          <mat-icon>account_circle</mat-icon>
+        </button>
+        <mat-menu #profileMenu="matMenu">
+          <button mat-menu-item><mat-icon>person</mat-icon> Profile</button>
+          <button mat-menu-item><mat-icon>settings</mat-icon> Settings</button>
+          <button mat-menu-item><mat-icon>logout</mat-icon> Logout</button>
+          <a mat-menu-item href="/sign-in">
+            <mat-icon>login</mat-icon> Sign In
+          </a>
+        </mat-menu>
+      </mat-toolbar>
 
-      <button mat-icon-button [matMenuTriggerFor]="profileMenu">
-        <mat-icon>account_circle</mat-icon>
-      </button>
-      <mat-menu #profileMenu="matMenu">
-        <button mat-menu-item><mat-icon>person</mat-icon> Profile</button>
-        <button mat-menu-item><mat-icon>settings</mat-icon> Settings</button>
-        <button mat-menu-item><mat-icon>logout</mat-icon> Logout</button>
-        <a mat-menu-item href="/sign-in">
-          <mat-icon>login</mat-icon> Sign In
-        </a>
-      </mat-menu>
-    </mat-toolbar>
+      <mat-sidenav-container class="sidenav-container">
+        <mat-sidenav
+          *ngIf="isLargeScreen"
+          mode="side"
+          [opened]="true"
+          [style.width]="sidenavWidth()"
+        >
+          <app-custom-sidenav [collapsed]="collapsed()"></app-custom-sidenav>
+        </mat-sidenav>
 
-    <mat-sidenav-container class="sidenav-container">
-      <mat-sidenav
-        *ngIf="isLargeScreen"
-        mode="side"
-        [opened]="true"
-        [style.width]="sidenavWidth()"
-      >
-        <app-custom-sidenav [collapsed]="collapsed()"></app-custom-sidenav>
-      </mat-sidenav>
+        <mat-sidenav
+          *ngIf="!isLargeScreen"
+          mode="over"
+          [opened]="!collapsed()"
+          [style.width]="collapsed() ? '0px' : '250px'"
+        >
+          <app-custom-sidenav [collapsed]="false"></app-custom-sidenav>
+        </mat-sidenav>
 
-      <mat-sidenav
-        *ngIf="!isLargeScreen"
-        mode="over"
-        [opened]="!collapsed()"
-        [style.width]="collapsed() ? '0px' : '250px'"
-      >
-        <app-custom-sidenav [collapsed]="false"></app-custom-sidenav>
-      </mat-sidenav>
+        <mat-sidenav-content
+          class="content"
+          [style.margin-left]="isLargeScreen ? sidenavWidth() : '0px'"
+        >
+          <router-outlet></router-outlet>
+        </mat-sidenav-content>
+      </mat-sidenav-container>
 
-      <mat-sidenav-content
-        class="content"
-        [style.margin-left]="isLargeScreen ? sidenavWidth() : '0px'"
-      >
-        <router-outlet></router-outlet>
-      </mat-sidenav-content>
-    </mat-sidenav-container>
-    <app-footer></app-footer>
+      <app-footer></app-footer>
+    </ng-container>
+
+    <ng-container *ngIf="!showLayout">
+      <router-outlet></router-outlet>
+    </ng-container>
   `,
   styles: [
     `
@@ -156,8 +159,18 @@ import { FooterComponent } from './components/footer/footer.component';
 })
 export class AppComponent implements OnInit {
   collapsed = signal(false);
+  showLayout = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    // Listen to route changes
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Define routes that should not show the layout
+        const excludedRoutes = ['/portfolio-details'];
+        this.showLayout = !excludedRoutes.includes(event.urlAfterRedirects);
+      }
+    });
+  }
 
   ngOnInit() {
     this.updateSidebarState();
